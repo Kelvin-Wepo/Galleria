28dab9d8730cf687252ed30af8da4cb5800cdcbb
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-05-30 00:28:54 +0300

    feat:this commit modifies admin.py

--- galleryApp/admin.py
+++ galleryApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Location, Category, Image
+
+# Register your models here.
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
+
 
 # Register your models here.

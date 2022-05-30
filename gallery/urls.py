d996fff1649b768335128aaf61fdb8d9d666be13
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-05-30 04:09:31 +0300

    feat: this commit modifies urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from sys import path
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include
-
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('photos.urls'))
+    path('', include('gallery.urls'))
 ]
